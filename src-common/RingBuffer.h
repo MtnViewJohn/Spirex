279616b6af7660f2878d03cc75b409435233e9a9
John W. Horigan <john@glyphic.com>
2017-01-16 09:28:08 -0800

    common - reindent ringbuffer and switch from int to unsigned to avoid undefined behavior

--- src-common/RingBuffer.h
+++ src-common/RingBuffer.h
@@ -1,6 +1,6 @@
 // Spirex Screensaver
 // ---------------------
-// Copyright (C) 2001, 2002, 2003 John Horigan
+// Copyright (C) 2001-2017 John Horigan
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-// 
+//
 // John Horigan can be contacted at john@glyphic.com or at
 // John Horigan, 1209 Villa St., Mountain View, CA 94041-1123, USA
 //
@@ -24,57 +24,56 @@
 #ifndef INCLUDED_RINGBUFFER
 #define INCLUDED_RINGBUFFER
 
-#include <assert.h>
+#include <cassert>
 #include <memory>
 
 template <class T>
-class RingBuffer 
+class RingBuffer
 {
-	std::unique_ptr<T[]> buffer;
-	int bufferSize, ringSize;
-	int pointer;
+    std::unique_ptr<T[]> buffer;
+    unsigned bufferSize, ringSize;
+    unsigned pointer;
 public:
-	RingBuffer():buffer(0),bufferSize(0),ringSize(0),pointer(0)	{}
-	RingBuffer(int sz, T init):buffer(0),bufferSize(0),ringSize(0),pointer(0)	
-		{resetBuffer(sz, init);}
+    RingBuffer(): bufferSize(0), ringSize(0), pointer(0)	 {}
+    RingBuffer(unsigned sz, T init): bufferSize(0), ringSize(0), pointer(0)
+    { resetBuffer(sz, init); }
     ~RingBuffer() = default;
-	void resetBuffer(int sz, T init);
-	void setSize(int sz);
-	void add(T item);
-	T tail(int index) const 
-		{ return buffer[(pointer + bufferSize - ringSize + index + 1) % bufferSize]; }
-	T get(int index) const	
-		{ return buffer[(pointer + bufferSize - index) % bufferSize]; }
-	int length() const		{ return ringSize; }
-private:
-	RingBuffer(const RingBuffer&);
-	RingBuffer& operator=(const RingBuffer&);
-		// not implemented, can't be copied or assigned
+    void resetBuffer(unsigned sz, T init);
+    void setSize(unsigned sz);
+    void add(T item);
+    T tail(unsigned index) const
+    { return buffer[(pointer + bufferSize - ringSize + index + 1) % bufferSize]; }
+    T get(unsigned index) const
+    { return buffer[(pointer + bufferSize - index) % bufferSize]; }
+    unsigned length() const      { return ringSize; }
+
+    RingBuffer(const RingBuffer&) = delete;
+    RingBuffer& operator=(const RingBuffer&) = delete;
 };
 
 template <class T>
-void RingBuffer<T>::resetBuffer(int sz, T init)
+void RingBuffer<T>::resetBuffer(unsigned sz, T init)
 {
     buffer = std::make_unique<T[]>(sz);
-	bufferSize = ringSize = sz;
-	for (int i = 0; i < bufferSize; i++) buffer[i] = init;
-	pointer = 0;
+    bufferSize = ringSize = sz;
+    for (unsigned i = 0; i < bufferSize; i++) buffer[i] = init;
+    pointer = 0;
 }
 
 template <class T>
 void RingBuffer<T>::add(T item)
 {
-	pointer = (pointer + 1) % bufferSize;
-	buffer[pointer] = item;
+    pointer = (pointer + 1) % bufferSize;
+    buffer[pointer] = item;
 }
 
 template <class T>
-void RingBuffer<T>::setSize(int sz)	
-{ 
+void RingBuffer<T>::setSize(unsigned sz)
+{
 #if DEBUG
-	assert(sz <= bufferSize); 
+    assert(sz <= bufferSize);
 #endif	
-	ringSize = sz; 
+    ringSize = sz; 
 }
 
 #endif // INCLUDED_RINGBUFFER
